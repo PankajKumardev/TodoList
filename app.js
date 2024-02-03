20b0cc4a98bd6f6038eb4004dd67093483b823e0
PankajKumardev <104948046+PankajKumardev@users.noreply.github.com>
2024-02-03 22:29:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+btn.addEventListener("click", function(){
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+    
+    let delbtn = document.createElement("button");
+    delbtn.innerText = "delete";    
+    delbtn.classList.add("delete");
+
+    item.appendChild(delbtn); 
+    ul.appendChild(item); 
+    inp.value = "";
+});
+ul.addEventListener("click", function (event) {
+    if(event.target.nodeName == "BUTTON") {
+        let listitem = event.target.parentElement;
+        listitem.remove();
+    }
+})
+// let delbtns = document.querySelectorAll(".delete");
+// for (delbtn1 of delbtns) {
+//     delbtn1.addEventListener("click", function() {
+//         let par = this.parentElement;
+//         par.remove;
+//     });
+// }
